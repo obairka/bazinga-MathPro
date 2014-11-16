b3dc17d572a34f0bad5b87a66722243263961d45
Baira Oydopova <obairka@gmail.com>
2014-11-17 01:47:33 +0600

    Add Simple action HomeController.Ratings
    Add Simple action HomeController.Ratings

--- MathPro.WebUI/Controllers/HomeController.cs
+++ MathPro.WebUI/Controllers/HomeController.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
+using MathPro.WebUI.Models;
 
 namespace MathPro.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController()
+        {
+           
+        }
+
+        public HomeController(ApplicationUserManager userManager)
+        {   
+            UserManager = userManager;
+        }
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -20,11 +49,16 @@ namespace MathPro.WebUI.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        //
+        // Rating
+        // GET:
+        public async Task<ActionResult> Ratings()
+        {
+            IList<UserProfileBriefViewModel> list = new List<UserProfileBriefViewModel>();
+            await UserManager.Users.ForEachAsync(u => list.Add(new UserProfileBriefViewModel(u)));                 
+            
+            return View(list);
         }
     }
 }
